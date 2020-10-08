f27a88f43da8d18409b6899f1caf4a893050a255
Timothy Herchen <timothy.herchen@gmail.com>
2020-10-08 03:59:58 -0700

    begin bigint

--- src/math/bigint/bigint.js
+++ src/math/bigint/bigint.js
@@ -1,3 +1,10 @@
+/**
+ * Big integers in JS! I would use the native implementation or JSBI, but I want a pretty customized setup for fast
+ * multiplication, division, et cetera. Also, this will be fun.
+ *
+ * We represent a big-integer with a Uint32Array, chosen for compactness (about twice as space-efficient as a normal array).
+ * The
+ */
 export class BigInt {
 
 }
