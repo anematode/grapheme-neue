eb3cef0a630d681a4934381e964a7f4e76ca7f39
Timothy Herchen <timothy.herchen@gmail.com>
2020-10-14 07:21:08 -0700

    b o l u s

--- src/core/bolus.js
+++ src/core/bolus.js
@@ -10,8 +10,7 @@
  * and if the function in question takes an absurdly long amount of time to execute, it cannot be terminated gracefully;
  * the entire worker needs to be terminated and then restarted.
  *
- * Thus, we use ES6 generators. Note that these can be transpiled, with some performance limitations, to ES5 generators
- * via Regenerator. A function like this is called a "bolus". Why? Because I like that word. Also, it makes it feel
+ * We use a generator-like object called a "bolus". Why? Because I like that word. Also, it makes it feel
  * like the evaluation of these expensive functions is like digestion. We consume a bolus and digest it asynchronously;
  * it's not like while we're digesting, we can't do anything else. We do get periodic interruptions—stomach cramps,
  * defecation—but it does not control our life. If digestion is taking too long, we can go to the doctor and get a
@@ -39,6 +38,30 @@
  * expensive updates.
  */
 
+// Find the sum of integers from 1 to n in bolus form
+export function testBolus (n) {
+  let i = 0
+  let sum = 0
+  let finished = false
+
+  return {
+    next() {
+      if (finished) return { value: undefined, done: true }
+
+      for (let j = 0; j <= 1e5; ++i, ++j) { // Sum at most 10000 values
+        sum += i
+
+        if (i === n) {
+          finished = true
+          return { value: sum, done: true }
+        }
+      }
+
+      return { value: i / n, done: false }
+    }
+  }
+}
+
 
 export function coatBolus (bolus, stepIndex, stepCount) {
 
@@ -52,16 +75,25 @@ class BolusTimeoutError extends Error {
 }
 
 /**
- * Digest a bolus directly, which is ideal for
- * @param bolus {Function} The bolus to evaluate, which may be a normal function or a generator.
- * @param params {Object} Extra parameters
- * @param params.args {Array} Array of arguments (enzymes) to supply the bolus
- * @param params.timeout {number} How many milliseconds to permit the function evaluation before raising a BolusTimeoutError
+ * A Bolus is any object with a next() function and potentially a cleanup() function. The cleanup() function is called
+ * only if the bolus is terminated early. If the bolus is cancelled after completing digestion, cleanup() is not called.
+ * next() returns { value: ..., done: false/true }. cleanup() is optional, and will not be called if the generator has
+ * finished. value is a number between 0 and 1.
+ * @typedef Bolus {Object}
+ * @property {function} next
+ * @property {function} cleanup
  */
-export function syncDigest (bolus, { args = [], timeout = -1 } = {}) {
-  const ingestedBolus = bolus(...args)
 
+/**
+ * Digest a bolus directly, which is ideal for
+ * @param bolus {Bolus} The bolus to evaluate, which may be a normal function or a generator.
+ * @param timeout {number} How many milliseconds to permit the function evaluation before raising a BolusTimeoutError
+ */
+export function syncDigest (bolus, timeout = -1) {
   if (timeout > 0 || timeout < 1e7) { // Do the timeout
+    // We could use performance.now(), but that function is about 10x slower. The issue now is 
+    const startTime = Date.now()
+
 
   }
 
